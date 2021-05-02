daaca3fe1abe7b74a4dd9e74f3d78d950560a2d0
Lenny <lenny.donnez@gmail.com>
2021-05-02 21:54:40 +0200

    refreshToken is optional

--- server/src/modules/authentication/dto/refreshToken.dto.ts
+++ server/src/modules/authentication/dto/refreshToken.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from "@nestjs/swagger"
 import { IsOptional, IsString } from "class-validator"
 
 export class RefreshTokenDto {
-  @ApiProperty()
+  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
   public refreshToken?: string
